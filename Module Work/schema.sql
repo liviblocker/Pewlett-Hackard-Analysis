ec0a01d0e698263aafa5d0396218a7da9eac5efa
liviblocker <liviblocker@gmail.com>
2020-10-18 13:08:51 -0500

    adding module work

--- Module Work/schema.sql
+++ Module Work/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+	 emp_no INT,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -33,19 +33,18 @@ CREATE TABLE salaries (
 );
 CREATE TABLE employment (
   emp_no INT NOT NULL,
-  dept_no VARCHAR NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no, title, from_date)
 );
-SELECT * FROM departments;
